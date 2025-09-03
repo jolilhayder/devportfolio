290f868e1cac47a28e399bb2a89981c80fdfa54e
Jolil Hayder <jolilhayder2002@gmail.com>
2025-09-03 03:44:11 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
-
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://jolilhayder.github.io',
-  base: '/devportfolio', // repo name
+  base: '/devportfolio',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
